f6d8bf816674166283aac4af8c7c3c620fc61f28
Gabriel Temtsen <gabrieltemtsen@gmail.com>
2023-08-21 07:55:41 +0100

    celo-composer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import "@rainbow-me/rainbowkit/styles.css";
 import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import celoGroups from "@celo/rainbowkit-celo/lists";
 import type { AppProps } from "next/app";
 import { configureChains, createConfig, WagmiConfig } from "wagmi";
 import { goerli, celoAlfajores } from "wagmi/chains";
@@ -17,11 +18,25 @@ const { chains, publicClient, webSocketPublicClient } = configureChains(
   [publicProvider()],
 );
 
-const { connectors } = getDefaultWallets({
-  appName: "RainbowKit App",
-  projectId: "fd65001eca3d65f4d845b7e9a7cba718",
+const projectId ="fd65001eca3d65f4d845b7e9a7cba718"
+
+const connectors = celoGroups({
   chains,
+  projectId,
+  appName:
+      (typeof document === "object" && document.title) || "Digi-afrika",
 });
+
+const appInfo = {
+  appName: "Digi-afrika",
+};
+
+
+// const { connectors } = getDefaultWallets({
+//   appName: "RainbowKit App",
+//   projectId: "fd65001eca3d65f4d845b7e9a7cba718",
+//   chains,
+// });
 
 const wagmiConfig = createConfig({
   autoConnect: true,
@@ -33,7 +48,9 @@ const wagmiConfig = createConfig({
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <WagmiConfig config={wagmiConfig}>
-      <RainbowKitProvider chains={chains}>
+      <RainbowKitProvider  appInfo={appInfo}
+                coolMode={true}
+                chains={chains}>
         <Component {...pageProps} />
       </RainbowKitProvider>
     </WagmiConfig>
